e51e1e61ad66ac152c4dd5c4146c7acc84bdb9da
“Reese <reesegreen2014@me.com>
2024-06-09 13:17:36 -0600

    logic to hide booking section upon logout

--- src/scripts.js
+++ src/scripts.js
@@ -24,36 +24,36 @@ const upcomingTripsHeader = document.querySelector('.sub-container1 .card-titles
 const pastTripsHeader = document.querySelector('.sub-container2 .card-titles');
 const pendingTripsHeader = document.querySelector('.sub-container3 .card-titles');
 const amountSpentHeader = document.querySelector('.sub-container4 .card-titles');
-
+const bookingSection = document.querySelector('.booking-section');
 bookTripButton.style.display = 'none';
 
 document.addEventListener('DOMContentLoaded', () => {
-loginButton.addEventListener('click', () => {
-    const isLoggedIn = loginButton.innerText === 'Logout';
-    if (isLoggedIn) {
-        handleLogout();
-    } else {
-        showLoginForm();
-    }
-});
-
-loginFormInner.addEventListener('submit', handleFormSubmission);
-
-if (tripRequestForm) {
-    tripRequestForm.addEventListener('submit', handleTripRequestSubmission);
-}
-
-fetchData(`${baseUrl}/destinations`)
-    .then(data => {
-        populateDestinationOptions(data.destinations);
-    })
-    .catch(error => {
-        console.error('Error fetching destinations:', error);
+    loginButton.addEventListener('click', () => {
+        const isLoggedIn = loginButton.innerText === 'Logout';
+        if (isLoggedIn) {
+            handleLogout();
+        } else {
+            showLoginForm();
+        }
     });
 
-if (calculateCostButton) {
-    calculateCostButton.addEventListener('click', calculateEstimatedCost);
-}
+    loginFormInner.addEventListener('submit', handleFormSubmission);
+
+    if (tripRequestForm) {
+        tripRequestForm.addEventListener('submit', handleTripRequestSubmission);
+    }
+
+    fetchData(`${baseUrl}/destinations`)
+        .then(data => {
+            populateDestinationOptions(data.destinations);
+        })
+        .catch(error => {
+            console.error('Error fetching destinations:', error);
+        });
+
+    if (calculateCostButton) {
+        calculateCostButton.addEventListener('click', calculateEstimatedCost);
+    }
 });
 
 const handleLogout = () => {
@@ -62,98 +62,101 @@ const handleLogout = () => {
     usernameInput.value = '';
     passwordInput.value = '';
     upcomingTripsHeader.textContent = 'Explore the World Stress-Free!';
-    upcomingTripsText.textContent = 'Discover over 50 dream destinations hassle-free with our expert planning. Let us handle every detail while you focus on the adventure!'
+    upcomingTripsText.textContent = 'Discover over 50 dream destinations hassle-free with our expert planning. Let us handle every detail while you focus on the adventure!';
     pastTripsHeader.textContent = 'Unlock Your Wanderlust with Ease!';
-    pastTripsElement.textContent = 'With a nominal 10% agent fee, enjoy the convenience of seamless travel planning. Leave the logistics to us and embrace the joy of exploration!'
+    pastTripsElement.textContent = 'With a nominal 10% agent fee, enjoy the convenience of seamless travel planning. Leave the logistics to us and embrace the joy of exploration!';
     pendingTripsHeader.textContent = 'Your Journey, Our Expertise';
     pendingTripsText.innerHTML = "From booking to boarding, we've got you covered. Trust our seasoned agents to curate your perfect getaway, ensuring a memorable experience every step of the way!";
     amountSpentHeader.textContent = "Let's get started!";
-    totalAmountSpentElement.textContent = 'Begin your journey by logging in. Your adventure awaits just a click away!'
+    totalAmountSpentElement.textContent = 'Begin your journey by logging in. Your adventure awaits just a click away!';
     bookTripButton.style.display = 'none';
-    loginButton.innerText = "Login"
-}
+    loginButton.innerText = "Login";
+    bookingSection.style.display = 'none';
+};
 
 const handleFormSubmission = (event) => {
-event.preventDefault();
+    event.preventDefault();
 
-const username = document.getElementById('username').value;
-const password = document.getElementById('password').value;
-const loginText = document.querySelector('.nav-login-button');
-if (validateCredentials(username, password)) {
-    const travelerId = extractTravelerId(username);
-    fetchUserData(travelerId);
-    loginText.innerText = 'Logout';
-    pendingTripsText.innerText = `You don't have any pending trips!`;
-    upcomingTripsText.innerText = `You don't have any upcoming trips!`;
-} else {
-    alert('Invalid username or password');
-}
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const loginText = document.querySelector('.nav-login-button');
+    if (validateCredentials(username, password)) {
+        const travelerId = extractTravelerId(username);
+        fetchUserData(travelerId);
+        loginText.innerText = 'Logout';
+        pendingTripsText.innerText = `You don't have any pending trips!`;
+        upcomingTripsText.innerText = `You don't have any upcoming trips!`;
+        bookingSection.style.display = 'block';
+    } else {
+        alert('Invalid username or password');
+    }
 };
 
 const fetchUserData = (travelerId) => {
-Promise.all([
-    fetchData(`${baseUrl}/trips`),
-    fetchData(`${baseUrl}/destinations`),
-    fetchData(`${baseUrl}/travelers/${travelerId}`)
-])
-.then(([tripsData, destinationsData]) => {
-    const trips = tripsData.trips || [];
-    const destinations = destinationsData.destinations || [];
-    const tripDetails = getTripDetailsForTraveler(travelerId, trips, destinations);
-    if (tripDetails) {
-        updateContainerHeaders(tripDetails)
-        updateTotalAmountSpent(tripDetails.totalAmountSpent);
-        updatePastTrips(tripDetails.pastTrips, destinations);
-        const bookTripButton = document.querySelector('.nav-book-button');
-        bookTripButton.style.display = 'block';
-    }
-    hideLoginForm();
-})
-.catch(error => {
-    console.error('Error fetching data:', error);
-});
+    Promise.all([
+        fetchData(`${baseUrl}/trips`),
+        fetchData(`${baseUrl}/destinations`),
+        fetchData(`${baseUrl}/travelers/${travelerId}`)
+    ])
+    .then(([tripsData, destinationsData]) => {
+        const trips = tripsData.trips || [];
+        const destinations = destinationsData.destinations || [];
+        const tripDetails = getTripDetailsForTraveler(travelerId, trips, destinations);
+        if (tripDetails) {
+            updateContainerHeaders(tripDetails);
+            updateTotalAmountSpent(tripDetails.totalAmountSpent);
+            updatePastTrips(tripDetails.pastTrips, destinations);
+            const bookTripButton = document.querySelector('.nav-book-button');
+            bookTripButton.style.display = 'block';
+        }
+        hideLoginForm();
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
 };
 
 const updateContainerHeaders = (tripDetails) => {
-upcomingTripsHeader.textContent = 'Your upcoming trips';
-pastTripsHeader.textContent = 'Your past trips';
-pendingTripsHeader.textContent = 'Your pending trips';
-amountSpentHeader.textContent = 'Amount Spent This year';
+    upcomingTripsHeader.textContent = 'Your upcoming trips';
+    pastTripsHeader.textContent = 'Your past trips';
+    pendingTripsHeader.textContent = 'Your pending trips';
+    amountSpentHeader.textContent = 'Amount Spent This year';
 };
 
 const populateDestinationOptions = (destinations) => {
-const destinationSelect = document.getElementById('destination');
-destinations.forEach(destination => {
-    const option = document.createElement('option');
-    option.value = destination.id;
-    option.textContent = destination.destination;
-    destinationSelect.appendChild(option);
-});
+    const destinationSelect = document.getElementById('destination');
+    destinations.forEach(destination => {
+        const option = document.createElement('option');
+        option.value = destination.id;
+        option.textContent = destination.destination;
+        destinationSelect.appendChild(option);
+    });
 };
 
 const calculateEstimatedCost = () => {
-const destinationSelect = document.getElementById('destination');
-const numTravelers = parseInt(document.getElementById('numTravelers').value);
-const duration = parseInt(document.getElementById('duration').value);
+    const destinationSelect = document.getElementById('destination');
+    const numTravelers = parseInt(document.getElementById('numTravelers').value);
+    const duration = parseInt(document.getElementById('duration').value);
+    
+    const selectedDestination = destinationSelect.options[destinationSelect.selectedIndex];
+    const destinationId = parseInt(selectedDestination.value);
 
-const selectedDestination = destinationSelect.options[destinationSelect.selectedIndex];
-const destinationId = parseInt(selectedDestination.value);
-
-fetchData(`${baseUrl}/destinations`)
-    .then(data => {
-        const destination = data.destinations.find(dest => dest.id === destinationId);
-        if (destination) {
-            const lodgingCost = destination.estimatedLodgingCostPerDay * duration;
-            const flightCost = destination.estimatedFlightCost
-            const totalCost = lodgingCost + flightCost;
-            const totalCostWithFee = totalCost + (totalCost * 0.10); // Adding 10% travel agent fee
-            document.getElementById('estimatedCost').value = `$${totalCostWithFee.toFixed(2)}`;
-        }
-    })
-    .catch(error => {
-        console.error('Error fetching destination:', error);
-    });
+    fetchData(`${baseUrl}/destinations`)
+        .then(data => {
+            const destination = data.destinations.find(dest => dest.id === destinationId);
+            if (destination) {
+                const lodgingCost = destination.estimatedLodgingCostPerDay * duration;
+                const flightCost = destination.estimatedFlightCostPerPerson * numTravelers;
+                const totalCost = lodgingCost + flightCost;
+                const totalCostWithFee = totalCost + (totalCost * 0.10); 
+                document.getElementById('estimatedCost').value = `$${totalCostWithFee.toFixed(2)}`;
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching destination:', error);
+        });
 };
+
 
 const handleTripRequestSubmission = (event) => {
     event.preventDefault();
@@ -200,7 +203,4 @@ const hideTripRequestForm = () => {
     const tripRequestForm = document.getElementById('tripRequestForm');
     tripRequestForm.classList.add('trip-request-form-hidden');
 };
-
-
-
 
